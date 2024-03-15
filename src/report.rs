844257b335edb90e499e92a2d16dce7edb03be60
roblabla <unfiltered@roblab.la>
2024-03-15 19:53:30 +0100

    Add compatibility with new schema

--- src/report.rs
+++ src/report.rs
@@ -8,34 +8,124 @@ use serde::Serialize;
 pub struct Report {
     pub version: String,
     pub vulnerabilities: Vec<Vulnerability>,
-    // pub remediations: Option<Vec<Remediation>>,
+    pub scan: Scan,
     pub dependency_files: Vec<DependencyFile>,
 }
 
 #[derive(Serialize, Debug, Default)]
-pub struct Vulnerability {
+pub struct Scan {
+    //#[serde(skip_serializing_if = "Vec::is_empty")]
+    //#[serde(default)]
+    //pub messages: Vec<Message>,
+    //pub options: Vec<Option>,
+    //pub primary_identifiers: Vec<PrimaryIdentifier>,
+    pub analyzer: Analyzer,
+    pub scanner: Scanner,
+    pub start_time: String,
+    pub end_time: String,
+    pub status: ScanStatus,
+    pub r#type: ScanType,
+}
+
+#[allow(dead_code)]
+#[derive(Serialize, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum ScanStatus {
+    Success,
+    Failure,
+}
+
+impl Default for ScanStatus {
+    fn default() -> Self {
+        ScanStatus::Success
+    }
+}
+
+#[derive(Serialize, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum ScanType {
+    DependencyScanning,
+}
+
+impl Default for ScanType {
+    fn default() -> Self {
+        ScanType::DependencyScanning
+    }
+}
+
+#[derive(Serialize, Debug, Default)]
+pub struct Analyzer {
+    pub id: String,
+    pub name: String,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub id: Option<String>,
-    pub category: String,
+    pub url: Option<String>,
+    pub vendor: Vendor,
+    pub version: String,
+}
+
+#[derive(Serialize, Debug, Default)]
+pub struct Scanner {
+    pub id: String,
+    pub name: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub url: Option<String>,
+    pub vendor: Vendor,
+    pub version: String,
+}
+
+#[derive(Serialize, Debug, Default)]
+pub struct Vendor {
+    pub name: String,
+}
+
+#[derive(Serialize, Debug, Default)]
+pub struct Vulnerability {
+    pub id: String,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub message: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub description: Option<String>,
-    pub cve: String,
     pub severity: Severity,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub confidence: Option<Confidence>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub solution: Option<String>,
-    pub scanner: Scanner,
     pub identifiers: Vec<Identifier>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub links: Option<Vec<Link>>,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    #[serde(default)]
+    pub cvss_vectors: Vec<CvssVector>,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    #[serde(default)]
+    pub links: Vec<Link>,
     //pub details: ???,
     //pub tracking: ???,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    #[serde(default)]
+    pub flags: Vec<Flag>,
     pub location: Location,
+}
+
+#[derive(Serialize, Debug, Default)]
+pub struct CvssVector {
+    pub vendor: String,
+    pub vector: String,
+}
+
+#[derive(Serialize, Debug, Default)]
+pub struct Flag {
+    pub r#type: FlagType,
+    pub origin: String,
+    pub description: String,
+}
+
+#[derive(Serialize, Debug)]
+#[serde(rename_all = "kebab-case")]
+pub enum FlagType {
+    FlaggedAsLikelyFalsePositive
+}
+
+impl Default for FlagType {
+    fn default() -> FlagType {
+        FlagType::FlaggedAsLikelyFalsePositive
+    }
 }
 
 #[allow(dead_code)]
@@ -46,31 +136,13 @@ pub enum Severity {
     Low,
     Medium,
     High,
-    Critical,   
+    Critical,
 }
 
 impl Default for Severity {
     fn default() -> Self {
         Self::Unknown
     }
-}
-
-#[allow(dead_code)]
-#[derive(Serialize, Debug)]
-pub enum Confidence {
-    Ignore,
-    Unknown,
-    Experimental,
-    Low,
-    Medium,
-    High,
-    Confirmed,
-}
-
-#[derive(Serialize, Debug, Default)]
-pub struct Scanner {
-    pub id: String,
-    pub name: String,
 }
 
 #[derive(Serialize, Debug, Default)]
